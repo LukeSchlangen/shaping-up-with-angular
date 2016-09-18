7460e90c4821ad027106979bc578ce1b76a8bdd2
lukeschlangen <lukeschlangen@gmail.com>
2016-09-18 07:55:16 -0500

    Adding ng-repeat to store products

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,14 +2,28 @@
   var storeApp = angular.module('StoreApp', [ ])
   // controllers are where we define our apps behavior by adding functions and values
   storeApp.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
+  var gems = [{
     name: 'Ruby',
     price: 2.95,
     description: 'This is a red gem!',
     canPurchase: true,
     soldOut: false
-  }
+  },
+  {
+    name: 'Saphire',
+    price: 5.95,
+    description: 'This is a blue gem!',
+    canPurchase: false,
+    soldOut: false
+  },
+  {
+    name: 'Emerald',
+    price: 12.95,
+    description: 'This is a green gem!',
+    canPurchase: true,
+    soldOut: true
+  }];
 })();
