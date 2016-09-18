7495fdd0a013591b76bba1f2638a0ebced78051e
lukeschlangen <lukeschlangen@gmail.com>
2016-09-18 07:42:15 -0500

    Adding StoreController

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,13 @@
-var app = angular.module('store', [ ]);
+(function(){
+  var storeApp = angular.module('StoreApp', [ ])
+  // controllers are where we define our apps behavior by adding functions and values
+  storeApp.controller('StoreController', function(){
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Ruby',
+    price: 2.95,
+    description: 'This is a red gem!'
+  }
+})();
